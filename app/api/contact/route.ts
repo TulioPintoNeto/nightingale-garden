89bff37923797aa48aecfa46c9092cec4dd56250
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2024-01-07 12:01:51 +0000

    feat: uncomment email action

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -9,10 +9,10 @@ export const POST = async (request: Request) => {
     if (isPayloadValid(payload)) {
       const { name, email, message } = payload;
 
-      // await sendEmail({
-      //   subject: `Website - Message from ${name} (${email})`,
-      //   message,
-      // });
+      await sendEmail({
+        subject: `Website - Message from ${name} (${email})`,
+        message,
+      });
     }
 
     return new Response(null, { status: 204 });
